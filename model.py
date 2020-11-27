efb847f606bfae3e06ef836f3a7cde1a4b976d77
Jan Joneš <jjones@outlook.cz>
2020-11-27 16:40:04 +0100

    Load Tensorflow Dataset

--- model.py
+++ model.py
@@ -31,6 +31,26 @@ bytes = np.asarray(PIL.Image.open(train_segments[0]))
 mask = PIL.Image.fromarray(bytes * 255)
 
 # %%
+
+
+def load_dataset(basedir, images, segments):
+    def transform(path):
+        image = tf.io.read_file(path)
+        image = tf.io.decode_png(image)
+        mask_path = tf.strings.regex_replace(path, images, segments)
+        mask = tf.io.read_file(mask_path)
+        mask = tf.io.decode_png(mask)
+        return image, mask
+
+    images_dir = os.path.join(basedir, images)
+    dataset = tf.data.Dataset.list_files(os.path.join(images_dir, '*.png'))
+    return dataset.map(transform)
+
+
+# %%
+train = load_dataset(DATASET_PATH, 'train', 'trainannot')
+
+# %%
 efficientnet_b0 = efficient_net.pretrained_efficientnet_b0(include_top=False)
 
 # %%
